a6594c1dd1ff7b7cc622bda31c375f9c2fc39e08
Erik Telepovský <erik.telepovsky@gmail.com>
2015-01-29 10:11:37 +0100

    translatable app name and permission type

--- permissions_widget/templatetags/permissions_widget_tags.py
+++ permissions_widget/templatetags/permissions_widget_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.translation import ugettext
 
 register = template.Library()
 
@@ -6,3 +7,7 @@ register = template.Library()
 @register.filter
 def get_item(d, key):
     return d.get(key, None)
+
+@register.filter(name='translate')
+def translate(text):
+    return ugettext(text)
