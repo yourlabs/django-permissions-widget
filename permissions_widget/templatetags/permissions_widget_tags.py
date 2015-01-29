e36af2f7aff678813aa91f1967c16e68e4224138
Erik Telepovský <erik.telepovsky@gmail.com>
2015-01-29 15:04:32 +0100

    translation without capture node

--- permissions_widget/templatetags/permissions_widget_tags.py
+++ permissions_widget/templatetags/permissions_widget_tags.py
@@ -12,32 +12,3 @@ def get_item(d, key):
 @register.filter(name='translate')
 def translate(text):
     return ugettext(text)
-
-
-@register.tag
-def capture(parser, token):
-    """
-    Capture contents of block into context
-    --------------------------------------
-
-    Use case: variable accessing based on current variable values.
-
-    {% capture foo %}{{ foo.value }}-suffix{% endcapture %}
-    {% if foo in bar %}{% endif %}
-
-    Created on Monday, February 2012 by Yuji Tomita
-    """
-    nodelist = parser.parse(('endcapture',))
-    parser.delete_first_token()
-    varname = token.contents.split()[1]
-    return CaptureNode(nodelist, varname)
-
-
-class CaptureNode(template.Node):
-    def __init__(self, nodelist, varname):
-        self.nodelist = nodelist
-        self.varname = varname
-
-    def render(self, context):
-        context[self.varname] = self.nodelist.render(context)
-        return ''
