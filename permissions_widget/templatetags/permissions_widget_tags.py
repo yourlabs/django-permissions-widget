e0d7cdc172a51274c41e7d0a1d746160ddf99f14
Erik Telepovský <erik.telepovsky@gmail.com>
2015-01-29 11:18:57 +0100

    translated permission name

--- permissions_widget/templatetags/permissions_widget_tags.py
+++ permissions_widget/templatetags/permissions_widget_tags.py
@@ -8,6 +8,36 @@ register = template.Library()
 def get_item(d, key):
     return d.get(key, None)
 
+
 @register.filter(name='translate')
 def translate(text):
     return ugettext(text)
+
+
+@register.tag
+def capture(parser, token):
+    """
+    Capture contents of block into context
+    --------------------------------------
+
+    Use case: variable accessing based on current variable values.
+
+    {% capture foo %}{{ foo.value }}-suffix{% endcapture %}
+    {% if foo in bar %}{% endif %}
+
+    Created on Monday, February 2012 by Yuji Tomita
+    """
+    nodelist = parser.parse(('endcapture',))
+    parser.delete_first_token()
+    varname = token.contents.split()[1]
+    return CaptureNode(nodelist, varname)
+
+
+class CaptureNode(template.Node):
+    def __init__(self, nodelist, varname):
+        self.nodelist = nodelist
+        self.varname = varname
+
+    def render(self, context):
+        context[self.varname] = self.nodelist.render(context)
+        return ''
